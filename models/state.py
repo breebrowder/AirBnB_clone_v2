993d92d09fc2abd7fa788108ab3e9aadc84b4fee
Simon Sun <sunmingyu.97@gmail.com>
2021-06-06 18:38:20 -0500

    adding city and state

--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import Column, String
+from os import getenv
+from models import storage
+from models.city import City
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """class for State inherited from BaseModel and Base"""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+        "City",
+        cascade="all,delete-orphan",
+        backref=backref("state", cascade="all,delete"))
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """gets Cities instances with state_id"""
+            return [v for k, v in storage.all(City).items()
+                    if v.state_id == self.id]

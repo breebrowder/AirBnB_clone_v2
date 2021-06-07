ea18d3b889af820c87ef3324de91b9868141ac0e
Simon Sun <sunmingyu.97@gmail.com>
2021-06-07 01:04:40 -0500

    check syntax

--- models/place.py
+++ models/place.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This is the place class"""
+from models import Amenity
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Float, Integer, ForeignKey, Table
 from sqlalchemy.orm import relationship, backref
@@ -14,14 +15,8 @@ class Place(BaseModel, Base):
                               Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False))
 
     __tablename__ = "places"
-    city_id = Column(String(60),
-                     ForeignKey("cities.id",
-                                ondelete="CASCADE"),
-                                nullable=False)
-    user_id = Column(String(60),
-                     ForeignKey("users.id",
-                                ondelete="CASCADE"),
-                                nullable=False)
+    city_id = Column(String(60), ForeignKey("cities.id", ondelete="CASCADE"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, default=0, nullable=False)

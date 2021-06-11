1db0159957c03c92dade4d35da6b98741bf9813e
breezybrow <browder.bree@gmail.com>
2021-06-11 18:57:40 +0000

    Prepare your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Setting up web servers for deployment of web static
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+sudo apt-get -y install nginx
+
+sudo service nginx start
+
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "Howdy!" | sudo tee /data/web_static/releases/test/index.html
+
+ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default

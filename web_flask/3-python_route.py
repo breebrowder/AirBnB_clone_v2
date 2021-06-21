d3f20d768f14cd7e034ce7fb137d9efe5cb3e8f1
breezybrow <browder.bree@gmail.com>
2021-06-21 23:57:02 +0000

    Update host and port

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,4 +27,4 @@ def p_text(text="is cool"):
     return "Python {}".format(text.replace('_', ' '))
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)

a5fd400ce71ce43df9a846983adb1b4dce3caac3
breezybrow <browder.bree@gmail.com>
2021-06-22 00:35:40 +0000

    Number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def display():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def p_text(text="is cool"):
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def num_route(n):
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', atrict_slashes=False)
+def num_temp_route(n):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)

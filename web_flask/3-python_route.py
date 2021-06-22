cc738720beae856b08b7fd721ac5ee7316f81c81
breezybrow <browder.bree@gmail.com>
2021-06-22 00:55:31 +0000

    Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -7,23 +7,27 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
+def greeting():
+    """ Method: return Hello """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb')
-def display():
+def hbnb():
+    """ Method: return HBNB """
     return "HBNB"
 
 
 @app.route('/c/<text>')
 def c_text(text):
+    """ Method: return C with value of text """
     return "C {}".format(text.replace('_', ' '))
 
 
 @app.route('/python/')
 @app.route('/python/<text>', strict_slashes=False)
-def p_text(text="is cool"):
+def py_text(text="is cool"):
+    """ Method: return Python with value of text """
     return "Python {}".format(text.replace('_', ' '))
 
 if __name__ == "__main__":

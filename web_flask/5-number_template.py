df5165d3c8c7485db521d9c3af3dcd431cad253b
breezybrow <browder.bree@gmail.com>
2021-06-22 00:46:15 +0000

    Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -7,33 +7,39 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
+def greeting():
+    """ Method: return Hello """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb')
-def display():
+def hbnb():
+    """ Method: return HBNB """
     return "HBNB"
 
 
 @app.route('/c/<text>')
 def c_text(text):
+    """ Method: return C with value of text """
     return "C {}".format(text.replace('_', ' '))
 
 
 @app.route('/python/')
 @app.route('/python/<text>', strict_slashes=False)
 def p_text(text="is cool"):
+    """ Method: return Python with value of text """
     return "Python {}".format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def num_route(n):
+    """ Method: returns n is a number """
     return "{} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>', atrict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def num_temp_route(n):
+    """ Method: return n in HTML """
     return render_template('5-number.html', n=n)
 
 
